8bfb0339ecf43cca41ea98b1b4fe41a8ff25f82a
Osama KH <320200112008@STU.TTU.EDU.JO>
2025-02-21 04:09:47 +0300

    update 1.4 1- modifiy csv file 2- apply new rules to user model 3- activate the Inherit view to user

--- __manifest__.py
+++ __manifest__.py
@@ -10,9 +10,9 @@
     'depends': ['base'],
     'data': [
         'security/ir.model.access.csv',
-	      'views/estate_property_views.xml',
-        'views/users_views.xml',
-	      'views/estate_menus.xml'
+	    'views/estate_property_views.xml',
+        'views/estate_menus.xml',
+        'views/users_views.xml'
       ],
 
     'installable': True,
--- models/res_users.py
+++ models/res_users.py
@@ -1,10 +1,9 @@
-#mdklfsdskfmc
 from odoo import models, fields
 
-class Respartner(models.Model):
+class ResUsers(models.Model):
     _inherit = "res.users"
 
-    property_ids = fields.One2many(
+    property_type_ids = fields.One2many(
         comodel_name='estate.property',  
         inverse_name='salesperson_id',   
         string='Properties'              
